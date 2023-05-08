cf9294cfb1ebb45c0e48f08127ecb4e3d1b0460b
Troy Squillaci <Zivia@remote.c21bestrealty.com>
2023-05-08 17:25:25 -0600

    Updated README and Install Script
    Signed-off-by: Troy Squillaci <Zivia@remote.c21bestrealty.com>

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
-Configuration for the following tools:
+# Dotfiles
+
+A collection of configuration files for my terminal environments.
+
+## Installation
+
+Download and install the MelsoLG font from [NerdFonts](https://www.nerdfonts.com/font-downloads).
+
+```bash
+git clone https://github.com/tmsquill/dotfiles.git
+cd dotfiles
+./install.sh
+```
+
+## Tools
 
 - [alacritty](https://github.com/alacritty/alacritty)
 - [tmux](https://github.com/tmux/tmux)
--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Git submodule installation.
+git submodule update --init --recursive
+
+# Git installed dependencies.
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 # Create the $XDG_CONFIG_HOME directory.
 mkdir -p $HOME/.config
 
@@ -8,3 +14,4 @@ CONFIG_DIR=$( dirname -- "$( readlink -f -- "$0"; )"; );
 ln -s $CONFIG_DIR/config/alacritty $HOME/.config/alacritty
 ln -s $CONFIG_DIR/config/nvim $HOME/.config/nvim
 ln -s $CONFIG_DIR/config/tmux $HOME/.config/tmux
+ln -s $CONFIG_DIR/config/zsh $HOME/.config/zsh
